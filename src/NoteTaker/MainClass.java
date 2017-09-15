0a3bdcf64eb3a9ef7a27bde0c348cd6a578fdcfa
Dean Moore <deanmoore009@gmail.com>
2017-09-15 23:48:01 +0100

    Changed the main frame

--- src/NoteTaker/MainClass.java
+++ src/NoteTaker/MainClass.java
@@ -1,8 +1,13 @@
 package NoteTaker;
 
 import javax.swing.*;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Text;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.font.TextAttribute;
+import java.util.Map;
 
 public class MainClass {
 	
@@ -13,6 +18,8 @@ public class MainClass {
 	private static JMenuItem menuItem;
 	private static JRadioButtonMenuItem rbMenuItem;
 	private static JCheckBoxMenuItem jcbMenuItem;
+	private static JLabel topLabel;
+	private static Font font;
 	
 	
 	public MainClass() {
@@ -25,6 +32,10 @@ public class MainClass {
 	}
 
 	public static void createWindow() {
+		
+		//creating the font
+		font = new Font("Book Antiqua", font.PLAIN, 20);	
+		
 		//creating the frame 
 		frame = new JFrame("PRO-Crastin8or");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -112,16 +123,40 @@ public class MainClass {
         mainPanel.setBounds(4, 3, 310, 200);
         mainPanel.setBackground(Color.WHITE);
         
+        //adding top title
+        topLabel = new JLabel("Tasks to do");
+        topLabel.setFont(font);
+
+        //adding an underline to the topLabel
+        makeFontUnderlined(topLabel);
+        
+     
+        
         //adding the menu bar and the main panel to the frame
         frame.setJMenuBar(menuBar);
+        mainPanel.add(topLabel);
         frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+        
                
         // pack and display the frame
         frame.pack();
+        frame.setResizable(false);
         frame.setVisible(true);
+        
+        
+        
+        
+        
+        
 	}
 	
 	
+	public static void makeFontUnderlined(JLabel aLabel) {
+		 Font font2 = aLabel.getFont();
+	     Map attributes = font2.getAttributes();
+	     attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+	     aLabel.setFont(font2.deriveFont(attributes));
+	}
 	
 	
 	
